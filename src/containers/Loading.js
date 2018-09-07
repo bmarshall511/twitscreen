af57806af0a7da5a0ab865fa3300c5a8209b4365
Ben Marshall <bmarshall511@gmail.com>
2018-09-06 21:45:31 -0500

    Build fix

--- src/containers/Loading.js
+++ src/containers/Loading.js
@@ -6,6 +6,7 @@ class Loading extends Component {
     return (
       <div className="modal">
         <h3 className="text-center">Twitscreen is booting up &mdash; {"shouldn't"} be much longer.</h3>
+        <p className="text-center">If this continues for more than a few minutes, double-check the <button data-toggle="offCanvas" className="text-link">"Twitscreen API URL"</button>. Be sure {"it's"} a valid endpoint. If the problem persists, please <a href="https://benmarshall.me/contact">report it</a>.</p>
       </div>
     );
   }
