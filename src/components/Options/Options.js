33870bab1af43ebed26f867883e5ab111dac442e
Ben Marshall <bmarshall511@gmail.com>
2018-09-07 08:27:43 -0500

    Build fix

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -13,31 +13,33 @@ class Options extends Component {
         <div className="grid-x grid-margin-x">
           <div className="cell">
             <ul className="accordion" data-accordion>
-            <li className="accordion-item is-active" data-accordion-item>
-              <a href="#" className="accordion-title">API Credentials</a>
-              <div className="accordion-content" data-tab-content>
-                <div className="grid-x grid-margin-x">
-                  <div className="cell">
-                    <p>For security reasons and to help prevent abuse, a registered Twitter consumer key and secret is required for Twitscreen to communicate with Twitter. Once {"you've"} <a href="https://dev.twitter.com/apps" target="_blank" rel="noopener noreferrer">registered an app</a>, enter those credentials below.</p>
+              {process.env !== 'production' &&
+                <li className="accordion-item is-active" data-accordion-item>
+                  <a href="#" className="accordion-title">API Credentials</a>
+                  <div className="accordion-content" data-tab-content>
+                    <div className="grid-x grid-margin-x">
+                      <div className="cell">
+                        <p>For security reasons and to help prevent abuse, a registered Twitter consumer key and secret is required for Twitscreen to communicate with Twitter. Once {"you've"} <a href="https://dev.twitter.com/apps" target="_blank" rel="noopener noreferrer">registered an app</a>, enter those credentials below.</p>
+                      </div>
+                      <div className="cell medium-6">
+                        <label>
+                          Consumer Key
+                          <input type="text" name="consumerKey" placeholder="Consumer Key (API Key)" value={state.consumerKey} onChange={handleInputChange} />
+                        </label>
+                      </div>
+                      <div className="cell medium-6">
+                        <label>
+                          Consumer Secret
+                          <input type="text" name="consumerSecret" placeholder="Consumer Secret (API Secret)" value={state.consumerSecret} onChange={handleInputChange} />
+                        </label>
+                      </div>
+                      <div className="cell">
+                        <p className="help-text">Get your Twitter application API key &amp; secret here: <a href="https://dev.twitter.com/apps" target="_blank" rel="noopener noreferrer">dev.twitter.com/apps</a></p>
+                      </div>
+                    </div>
                   </div>
-                  <div className="cell medium-6">
-                    <label>
-                      Consumer Key
-                      <input type="text" name="consumerKey" placeholder="Consumer Key (API Key)" value={state.consumerKey} onChange={handleInputChange} />
-                    </label>
-                  </div>
-                  <div className="cell medium-6">
-                    <label>
-                      Consumer Secret
-                      <input type="text" name="consumerSecret" placeholder="Consumer Secret (API Secret)" value={state.consumerSecret} onChange={handleInputChange} />
-                    </label>
-                  </div>
-                  <div className="cell">
-                    <p className="help-text">Get your Twitter application API key &amp; secret here: <a href="https://dev.twitter.com/apps" target="_blank" rel="noopener noreferrer">dev.twitter.com/apps</a></p>
-                  </div>
-                </div>
-              </div>
-            </li>
+                </li>
+              }
               <li className="accordion-item" data-accordion-item>
                 <a href="#" className="accordion-title">Filters</a>
                 <div className="accordion-content" data-tab-content>
@@ -90,29 +92,37 @@ class Options extends Component {
                 <a href="#" className="accordion-title">Advanced Options</a>
                 <div className="accordion-content" data-tab-content>
                   <div className="grid-x grid-margin-x">
-                    <div className="cell medium-4">
-                      <label>
-                        Twitscreen API URL
-                        <input type="url" name="apiUrl" placeholder="ex. https://benmarshall.me/api.php" value={state.apiUrl} onChange={handleInputChange} />
-                      </label>
-                      <p className="help-text">Enter the URL location of the Twitscreen API.</p>
-                    </div>
-                    <div className="cell medium-4">
-                      <label>
-                        Callback URL
-                        <input type="url" name="callback" placeholder="ex. https://trekific.com/twitscreen/build" value={state.callback} onChange={handleInputChange} />
-                      </label>
-                      <p className="help-text">Enter the URL location of the Twitscreen API.</p>
-                    </div>
-                    <div className="cell medium-4">
-                      <label>
-                        Pause
-                        <input type="number" name="pause" value={state.pause} onChange={handleInputChange} />
-                      </label>
-                      <p className="help-text">Number of seconds to display a tweet.</p>
-                    </div>
+                    {process.env !== 'production' &&
+                      <div className="cell">
+                        <div className="grid-x grid-margin-x">
+                          <div className="cell medium-4">
+                            <label>
+                              Twitscreen API URL
+                              <input type="url" name="apiUrl" placeholder="ex. https://benmarshall.me/api.php" value={state.apiUrl} onChange={handleInputChange} />
+                            </label>
+                            <p className="help-text">Enter the URL location of the Twitscreen API.</p>
+                          </div>
+                          <div className="cell medium-4">
+                            <label>
+                              Callback URL
+                              <input type="url" name="callback" placeholder="ex. https://trekific.com/twitscreen/build" value={state.callback} onChange={handleInputChange} />
+                            </label>
+                            <p className="help-text">Enter the URL location of the Twitscreen API.</p>
+                          </div>
+                          <div className="cell medium-4">
+                            <label>
+                              Pause
+                              <input type="number" name="pause" value={state.pause} onChange={handleInputChange} />
+                            </label>
+                            <p className="help-text">Number of seconds to display a tweet.</p>
+                          </div>
+                          <div className="cell">
+                            <hr />
+                          </div>
+                        </div>
+                      </div>
+                    }
                     <div className="cell">
-                      <hr />
                       <p className="text-center"><button className="button" onClick={reset}>Reset Twitscreen</button> <em>&mdash; deauthorizes Twitscreen & clears locally cached data</em></p>
                     </div>
                   </div>
